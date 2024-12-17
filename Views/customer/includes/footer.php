15daae7306960056f3e57b2b6747adbdb2861530
taka-duarr <busfirman9@gmail.com>
2024-12-18 06:41:28 +0700

    add footer

---
+++ Views/customer/includes/footer.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Zay Shop - Product List Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <base href="http://tugas_akhir.test/" />
+
+    <link rel="apple-touch-icon" href="Views/customer/assets/img/apple-icon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="Views/customer/assets/img/favicon.ico">
+
+    <link rel="stylesheet" href="Views/customer/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Views/customer/assets/css/templatemo.css">
+    <link rel="stylesheet" href="Views/customer/assets/css/custom.css">
+
+    <!-- Load fonts style after rendering the layout styles -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
+    <link rel="stylesheet" href="Views/customer/assets/css/fontawesome.min.css">
+
+<footer class="bg-black" id="tempaltemo_footer">
+        <div class="container">
+            <div class="row">
+
+            <div class="col-md-4 pt-5">
+    <h2 class="h2 text-light border-bottom pb-3 border-light logo">E-Kelontong</h2>
+    <ul class="list-unstyled text-light footer-link-list">
+        <li class="d-flex align-items-start">
+            <i class="fas fa-map-marker-alt fa-fw me-2"></i>
+            <div>Jl. Sigma no 27, desa skibidi, kecamatan rizz kabupaten singapura barat, jawa timur, Indonesia</div>
+        </li>
+    </ul>
+</div>
+
+
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Contact Us</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                    <li>
+                            <i class="fa fa-phone fa-fw"></i>
+                            <a class="text-decoration-none" href="tel:081234567890">081234567890</a>
+                        </li>
+                    <li>
+                            <i class="fa fa-envelope fa-fw"></i>
+                            <a class="text-decoration-none" href="mailto:yasinmart@gmail.com">yasinmart@gmail.com</a>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Further Info</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li><a class="text-decoration-none" href="Views/customer/board.php">Beranda</a></li>
+                        <li><a class="text-decoration-none" href="Views/customer/about.php">Tentang kami</a></li>
+                        <li><a class="text-decoration-none" href="Views/customer/contact.php">Lokasi</a></li>
+                    </ul>
+                </div>
+
+               
+
+                
+
+            </div>
+
+            <div class="row text-light mb-4">
+                <div class="col-12 mb-3">
+                    <div class="w-100 my-3 border-top border-light"></div>
+                </div>
+                <div class="col-auto me-auto">
+                    <ul class="list-inline text-left footer-icons">
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="http://facebook.com/"><i class="fab fa-facebook-f fa-lg fa-fw"></i></a>
+                        </li>
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="https://www.instagram.com/"><i class="fab fa-instagram fa-lg fa-fw"></i></a>
+                        </li>
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="https://twitter.com/"><i class="fab fa-twitter fa-lg fa-fw"></i></a>
+                        </li>
+                        
+                    </ul>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="w-100 bg-black py-3">
+           
+        </div>
+
+    </footer>
+    <!-- End Footer -->
+
+    <!-- Start Script -->
+    <script src="assets/js/jquery-1.11.0.min.js"></script>
+    <script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/templatemo.js"></script>
+    <script src="assets/js/custom.js"></script>
+
+    </body>
+
+    </html>
--- Views/customer/shop.php
+++ Views/customer/shop.php
@@ -111,91 +111,13 @@ https://templatemo.com/tm-559-zay-Views/customer/shop
 
 
 
-    <!-- Start Footer -->
-    <footer class="bg-black" id="tempaltemo_footer">
-        <div class="container">
-            <div class="row">
+    <body>
+    <!-- Start Top Nav -->
+    
+    <?php include 'includes/footer.php'; ?>
 
-            <div class="col-md-4 pt-5">
-    <h2 class="h2 text-light border-bottom pb-3 border-light logo">E-Kelontong</h2>
-    <ul class="list-unstyled text-light footer-link-list">
-        <li class="d-flex align-items-start">
-            <i class="fas fa-map-marker-alt fa-fw me-2"></i>
-            <div>Jl. Sigma no 27, desa skibidi, kecamatan rizz kabupaten singapura barat, jawa timur, Indonesia</div>
-        </li>
-    </ul>
-</div>
-
-
-
-                <div class="col-md-4 pt-5">
-                    <h2 class="h2 text-light border-bottom pb-3 border-light">Contact Us</h2>
-                    <ul class="list-unstyled text-light footer-link-list">
-                    <li>
-                            <i class="fa fa-phone fa-fw"></i>
-                            <a class="text-decoration-none" href="tel:081234567890">081234567890</a>
-                        </li>
-                    <li>
-                            <i class="fa fa-envelope fa-fw"></i>
-                            <a class="text-decoration-none" href="mailto:yasinmart@gmail.com">yasinmart@gmail.com</a>
-                        </li>
-                    </ul>
-                </div>
-
-
-                <div class="col-md-4 pt-5">
-                    <h2 class="h2 text-light border-bottom pb-3 border-light">Further Info</h2>
-                    <ul class="list-unstyled text-light footer-link-list">
-                        <li><a class="text-decoration-none" href="Views/customer/board.php">Home</a></li>
-                        <li><a class="text-decoration-none" href="Views/customer/about.php">About Us</a></li>
-                        <li><a class="text-decoration-none" href="https://maps.app.goo.gl/hGEjCLpvxttXM9qd8">Shop Locations</a></li>
-                        
-                        <li><a class="text-decoration-none" href="Views/customer/contact.php">Contact</a></li>
-                    </ul>
-                </div>
-
-               
-
-                
-
-            </div>
-
-            <div class="row text-light mb-4">
-                <div class="col-12 mb-3">
-                    <div class="w-100 my-3 border-top border-light"></div>
-                </div>
-                <div class="col-auto me-auto">
-                    <ul class="list-inline text-left footer-icons">
-                        <li class="list-inline-item border border-light rounded-circle text-center">
-                            <a class="text-light text-decoration-none" target="_blank" href="http://facebook.com/"><i class="fab fa-facebook-f fa-lg fa-fw"></i></a>
-                        </li>
-                        <li class="list-inline-item border border-light rounded-circle text-center">
-                            <a class="text-light text-decoration-none" target="_blank" href="https://www.instagram.com/"><i class="fab fa-instagram fa-lg fa-fw"></i></a>
-                        </li>
-                        <li class="list-inline-item border border-light rounded-circle text-center">
-                            <a class="text-light text-decoration-none" target="_blank" href="https://twitter.com/"><i class="fab fa-twitter fa-lg fa-fw"></i></a>
-                        </li>
-                        
-                    </ul>
-                </div>
-                
-            </div>
-        </div>
-
-        <div class="w-100 bg-black py-3">
-           
-        </div>
-
-    </footer>
-    <!-- End Footer -->
-
-    <!-- Start Script -->
-    <script src="assets/js/jquery-1.11.0.min.js"></script>
-    <script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/templatemo.js"></script>
-    <script src="assets/js/custom.js"></script>
-    <!-- End Script -->
+    <script src="Views/customer/assets/js/bootstrap.bundle.min.js"></script>
+    </body>
 </body>
 
 </html>
