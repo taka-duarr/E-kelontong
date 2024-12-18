6bf1af9e471eda3f47ad462131eb95293c0ea4a0
taka-duarr <busfirman9@gmail.com>
2024-12-18 07:36:35 +0700

    edit about, location

--- Views/customer/board.php
+++ Views/customer/board.php
@@ -95,19 +95,23 @@ https://templatemo.com/tm-559-zay-shop
 
 </script>
 
-<div class="col-lg-6 mb-0 d-flex justify-content-center align-items-center" style="height: 100vh;">
-    <div class="text-align-left text-center">
-        <h1 class="h1 text-success"><b>Zay</b> eCommerce</h1>
-        <h3 class="h2">Tiny and Perfect eCommerce Template</h3>
+<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <div class="text-center" style="max-width: 50%; word-wrap: break-word;">
+        <h1 class="h1 text-black"><b>E-</b> Kelontong</h1>
+        <h3 class="h2">Selamat datang di E-Kelontong</h3>
         <p>
-            Zay Shop is an eCommerce HTML5 CSS template with the latest version of Bootstrap 5 (beta 1). 
-            This template is 100% free provided by <a rel="sponsored" class="text-success" href="https://templatemo.com" target="_blank">TemplateMo</a> website. 
-            Image credits go to <a rel="sponsored" class="text-success" href="https://stories.freepik.com/" target="_blank">Freepik Stories</a>,
-            <a rel="sponsored" class="text-success" href="https://unsplash.com/" target="_blank">Unsplash</a>, and
-            <a rel="sponsored" class="text-success" href="https://icons8.com/" target="_blank">Icons 8</a>.
+        E-Kelontong merupakan solusi belanja kebutuhan sehari-hari yang mudah dan praktis.
+        Kami menyediakan berbagai macam produk berkualitas dengan harga terjangkau untuk memenuhi kebutuhan Anda.
+        </p>
+        <p>
+
+        Nikmati pengalaman berbelanja online yang cepat, aman, dan nyaman hanya di E-Kelontong.
+        Temukan promo menarik setiap harinya dan pastikan kebutuhan rumah tangga Anda selalu terpenuhi! 
         </p>
     </div>
 </div>
+
+
 
 
 
